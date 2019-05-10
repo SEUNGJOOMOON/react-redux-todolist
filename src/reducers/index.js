605401e4e7215ab088c05849e5a2ea34c5a35039
nick901106 <nick901106@gmail.com>
2019-05-10 10:12:59 +0900

    test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,17 @@
-import { ADD_TODO } from "../actions";
+import { ADD_TODO, REMOVE_TODO } from "../actions";
 
 export default (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
+        console.log(...state);
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text
+        },
+      ];
+    case REMOVE_TODO:
       return [
         ...state,
         {
